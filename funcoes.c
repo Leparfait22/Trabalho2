91938fcd295642b2af306d7985a57c2fbde41678
Leparfait <helomeranda@gmail.com>
2023-06-16 11:37:45 -0300

    trabalhando com menor numero e menor indice

--- funcoes.c
+++ funcoes.c
@@ -128,6 +128,82 @@ MenorElemento Menores(int vetor[], int tamanho) {
     return menor;
 }
 
+void particionarVetor(int vetor[], int tamamnho){
+    int bloco[MAX_IN_MEMORY];
+    char nomesEntrada[MAX_IN_MEMORY][15];
+    char nomeSaida[MAX_IN_MEMORY][15];
+    FILE *arquivosEntrada[MAX_IN_MEMORY];
+    FILE *arquivosSaida[MAX_IN_MEMORY];
+    FILE *arquivo;
+    int vetTemp[5];
+    int leitor;
+    int i= 0;
+    int j = 0;
+    int t = 1;
+    int cont = 0;
+    int k = 1;
+    int tamVetorPrincipal = tamamnho / sizeof(vetor[0]);
+
+    while (1){ 
+
+        if(i == (tamVetorPrincipal/MAX_IN_MEMORY)){
+            break;
+        }
+        if(i< MAX_IN_MEMORY){
+            sprintf(nomesEntrada[i], "entrada%d.txt", i + 1);
+            arquivosEntrada[i] = fopen(nomesEntrada[i], "w");
+            if (arquivosEntrada[i] == NULL) {
+                return 1;
+            }
+            
+        }
+        // arquivo intermediário já criado
+        int inicio = MAX_IN_MEMORY * i;
+        int limite = inicio + MAX_IN_MEMORY - 1;
+
+        // preenchendo o bloco com elementos
+        for (int p = inicio; p <= limite; p++) {
+            bloco[p - inicio] = vetor[p];
+        }
+
+        // ordenando o bloco
+        quickSort(bloco, MAX_IN_MEMORY - 1);
+
+        // verificando se o bloco foi criado com sucesso e ordenado
+        //for (int v = 0; v < MAX_IN_MEMORY; v++) {
+          //  printf("%d\n", bloco[v]);
+        //}
+
+        // escrevendo o bloco ordenado no arquivo
+        for (k = 0; k < MAX_IN_MEMORY; k++) {
+            if(i < MAX_IN_MEMORY){
+                fprintf(arquivosEntrada[i], "%d\n", bloco[k]);
+            }else{
+                fprintf(arquivosEntrada[i-MAX_IN_MEMORY*(i/MAX_IN_MEMORY)], "%d\n", bloco[k]);
+            }
+        }
+        // se ainda sobra elemento no vetro principal 
+        //ele é escrito na entrada final do arquivo
+        if(i == (tamVetorPrincipal/MAX_IN_MEMORY)-1){
+            int elementoRestante = tamVetorPrincipal -((i+1) * MAX_IN_MEMORY);
+            if(elementoRestante > 0){
+                int vetorRestante[elementoRestante];
+                for (int p = 0; p < elementoRestante; p++) {
+                    vetorRestante[p] = vetor[(i+1)*MAX_IN_MEMORY+p];
+                }
+                for(int r = 0;r < elementoRestante;r++){
+                    fprintf(arquivosEntrada[(i/MAX_IN_MEMORY)-1],"%d\n", vetorRestante[r]);
+                }
+            }
+        } 
+        i++;  
+    }
+    for (i = 0; i < TAMANHO_VETOR; i++) {
+        fclose(arquivosEntrada[i]);
+    }
+
+}
+
 /* char* nomeArquivo(int numero) {
     char* nome_arquivo = (char*)malloc(sizeof(char) * 15); // Tamanho máximo necessário é 13: "entradaX.txt\0"
     sprintf(nome_arquivo, "entrada%d.txt", numero);
@@ -141,94 +217,81 @@ int main() {
     FILE *arquivosEntrada[MAX_IN_MEMORY];
     FILE *arquivosSaida[MAX_IN_MEMORY];
     FILE *arquivo;
+    FILE *arquivos[MAX_IN_MEMORY];
     int vetTemp[5];
     int leitor;
-    int vetor[TAMANHO_VETOR];
     int i= 0;
     int j = 0;
     int t = 1;
     int cont = 0;
-    int k = 1;
+    int k = 1; 
+    int matriz[MAX_IN_MEMORY][MAX_IN_MEMORY];
+    int passagem = 1;
+    int menorNumero,s;
+
 
     
     
+     int chaves1[] = {3, 98, 61, 62,  1, 99, 57, 98, 58, 18, 59, 35, 82, 91, 52, 95, 46,
+        69, 48, 82, 52,  4, 87, 38, 44, 42, 72,  2, 95, 84, 34, 40, 23, 11, 75,
+        37, 62, 96, 12, 22, 56, 20, 64,  2, 30, 48,  6, 19, 34, 31};
     /* int chaves1[] = {3, 98, 61, 62,  1, 99, 57, 98, 58, 18, 59, 35, 82, 91, 52, 95, 46,
         69, 48, 82, 52,  4, 87, 38, 44, 42, 72,  2, 95, 84, 34, 40, 23, 11, 75,
-        37, 62, 96, 12, 22, 56, 20, 64,  2, 30, 48,  6, 19, 34, 31}; */
-    int chaves1[] = {3, 98, 61, 62,  1, 99, 57, 98, 58, 18, 59, 35, 82, 91, 52, 95, 46,
-        69, 48, 82, 52,  4, 87, 38, 44, 42, 72,  2, 95, 84, 34, 40, 23, 11, 75,
-        37, 62, 96, 12, 22, 56, 20, 64,  2, 30, 48,  6, 19, 34, 31, 51, 4, 9};
+        37, 62, 96, 12, 22, 56, 20, 64,  2, 30, 48,  6, 19, 34, 31, 51, 4, 9}; */
     int chaves2[] = {57, 91, 21, 89, 72, 74, 46, 61, 26, 50, 85, 76, 53, 22, 15, 40, 90, 2, 94, 54};
     int chaves3[] = {9,  2, 15, 19, 10, 14,  4, 13,  2, 19};
     int chaves4[] = {178, 231, 244, 292, 321, 356, 389, 421, 482, 488, 490, 502, 546, 641, 694, 786, 841, 890, 899, 922};
-    
-    int tamVetorPrincipal = (int)sizeof(chaves1)/sizeof(int);
-    printf("Numero de elemento : %d\n",tamVetorPrincipal);
+    int tamanho = sizeof(chaves1);
+    particionarVetor(chaves1,tamanho);
 
-    while (1){ 
+    /* arquivos[0] = fopen("entrada1.txt", "r");
+    arquivos[1] = fopen("entrada2.txt", "r");
+    arquivos[2] = fopen("entrada3.txt", "r");
+    arquivos[3] = fopen("entrada4.txt", "r");
+    arquivos[4] = fopen("entrada5.txt", "r"); */
 
-        if(i == (tamVetorPrincipal/MAX_IN_MEMORY)){
-            break;
-        }
-        printf("I = %d\n",i);
+    for(int i = 0;i<MAX_IN_MEMORY;i++){
         if(i< MAX_IN_MEMORY){
             sprintf(nomesEntrada[i], "entrada%d.txt", i + 1);
-            arquivosEntrada[i] = fopen(nomesEntrada[i], "w");
-            if (arquivosEntrada[i] == NULL) {
-                printf("Não foi possível criar o arquivo %s.\n", nomesEntrada[i]);
-                return 1;
+            //printf("%s\n",nomesEntrada[i]);
+        }
+        arquivos[i] = fopen(nomesEntrada[i], "r");
+        if (arquivos[i] == NULL) {
+            printf("Erro ao abrir o arquivo\n");
+            return 1;
+        }
+    }
+    for (int i = 0; i < MAX_IN_MEMORY; i++) {
+        for (int j = 0; j < MAX_IN_MEMORY; j++) {
+            fscanf(arquivos[i], "%d", &matriz[i][j]);
+        }
+    }
+    for(int j = 0;j<MAX_IN_MEMORY;j++){
+            printf("Arquivo %d\n",i+1);
+            for(k=0;k<MAX_IN_MEMORY;k++){
+                printf("\t%d -",matriz[j][k]);
+            }
+    }
+    int menorIndice = 0;
+    for (j = 0; j < MAX_IN_MEMORY; j++) {
+        //menorIndice = 0;
+        menorNumero = matriz[menorIndice][j];
+        
+
+        // Encontra o menor número da posição atual
+        for (i = 1; i < MAX_IN_MEMORY; i++) {
+            if (matriz[i][j] < menorNumero) {
+                menorNumero = matriz[i][j];
+                menorIndice = i;
             }
             else{
-                printf("%s\n", nomesEntrada[i]);
+                menorIndice ++;
             }
-        }
-        // arquivo intermediário já criado
-        int inicio = MAX_IN_MEMORY * i;
-        int limite = inicio + MAX_IN_MEMORY - 1;
+                    printf("Menor indice= %d\n",menorIndice);
+                    printf("Menor numero : %d\n",menorNumero);
 
-        // preenchendo o bloco com elementos
-        for (int p = inicio; p <= limite; p++) {
-            bloco[p - inicio] = chaves1[p];
         }
 
-        // ordenando o bloco
-        quickSort(bloco, MAX_IN_MEMORY - 1);
-
-        // verificando se o bloco foi criado com sucesso e ordenado
-        printf("verificacao do bloco\n");
-        for (int v = 0; v < MAX_IN_MEMORY; v++) {
-            printf("%d\n", bloco[v]);
-        }
-
-        // escrevendo o bloco ordenado no arquivo
-        printf("Arquivo para escrever %s\n", nomesEntrada[i]);
-        for (k = 0; k < MAX_IN_MEMORY; k++) {
-            if(i < MAX_IN_MEMORY){
-                fprintf(arquivosEntrada[i], "%d\n", bloco[k]);
-            }else{
-                printf("2a i = %d\n",i);
-                fprintf(arquivosEntrada[i-MAX_IN_MEMORY*(i/MAX_IN_MEMORY)], "%d\n", bloco[k]);
-            }
-        }
-        // se ainda sobra elemento no vetro principal 
-        //ele é escrito na entrada final do arquivo
-        if(i == (tamVetorPrincipal/MAX_IN_MEMORY)-1){
-            int elementoRestante = tamVetorPrincipal -((i+1) * MAX_IN_MEMORY);
-            printf("Elemento retante = %d\n",elementoRestante);
-            if(elementoRestante > 0){
-                int vetorRestante[elementoRestante];
-                for (int p = 0; p < elementoRestante; p++) {
-                    vetorRestante[p] = chaves1[(i+1)*MAX_IN_MEMORY+p];
-                }
-                for(int r = 0;r < elementoRestante;r++){
-                    fprintf(arquivosEntrada[(i/MAX_IN_MEMORY)-1],"%d\n", vetorRestante[r]);
-                }
-            }
-        } 
-        i++;  
     }
-    
-
-
     return 0;
 }
